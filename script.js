edd2cf6c1925c76de8fef27e6cd5920b72cf5002
Tom Greener <tgreener@hotmail.co.uk>
2016-08-02 02:00:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 //Collects all the paragraphs <p> in the page and puts them into an array called bodyText
-
 var bodyText = document.body.getElementsByTagName("p");
-var words = ["Wordpress", "CSS", "test"];
+
+//Reads the databse.js file into an array
+var words = JSON.parse(keywordsDatabase);
 
 for(y = 0; y < bodyText.length; y++) {
-    for(x = 0; x < words.length; x++) {        
-        var word = words[x];
-        //The "g" in the "gi" stands for global, meaning that all occurences of a keyword will be labelled instead of only the first occurence
-        var keyword = new RegExp("\\b(" + word + ")\\b", "gi");
+    for(x = 0; x < words.keywords.length; x++) {        
+        var word = words.keywords[x];
+        
+        //The "g" in the "gi" stands for global, meaning that all occurences of a keyword will be labelled instead of only the first occurence. The "i" means that the search is not case sensitive.
+        var keyword = new RegExp("\\b(" + word.name + ")\\b", "gi");
         
         if (keyword.exec(bodyText[y].textContent)) {
-            var spanString = "<span class='jargon-bust-outline'>" + word + "</span>";
+            var spanString = "<span class='scout-js-outline' data-title='" + word.title + "' data-description='" + word.description + "' data-url='" + word.url + "'>" + word.name + "</span>";
             bodyText[y].innerHTML = bodyText[y].innerHTML.replace(keyword, spanString);
         }
     }
@@ -18,14 +20,15 @@ for(y = 0; y < bodyText.length; y++) {
 
 // Once the keyword check has finished, we collect all the keywords found in an array by searching for the class that we attatched to the <span> tag above
 
-var highlightedWords = document.body.getElementsByClassName("jargon-bust-outline");
+var highlightedWords = document.body.getElementsByClassName("scout-js-outline");
 
-// Then we cycle through each span and add Event Listeners which check for mouse hover and mouse leave - when the mouse hovers over the keyword label, we call the createForm method and pass it the labels co-ordinates in order to render a jargon explaination form on the page in the correct place. When the mouse leaves the keyword label, we call the killForm function to kill all the forms on the page (this massacre is okay as there will only be one form active at a time!)
+//The three variables defined below are used to track the creation of the pop-up form outisde of the individual functions - formActive tracks if a pop-up is visible, okayToKillForm is used to give permission to kill open pop-ups and highlightedKeyword contains the object of the current pop-up's respective keyword.
 
-// This code section, and the line above, could/should be applied to the <span> as we create it in the previous code block. This would probably me much more efficient, as it saves us querying and looping through the DOM again.
-
-var okayToKillForm = true;
 var formActive = false;
+var okayToKillForm = true;
+var highlightedKeyword = null;
+
+// So now the nitty gritty - we cycle through each keywordn span and add Event Listeners which check for mouse hover and mouse leave - when the mouse hovers over the keyword span, we call the createForm method and pass it the keyword object so that we can later determine the keyword span's position and render the informational pop-up in the correct place on the page. When the mouse leaves the keyword span, we call the killForm function to kill all the forms on the page (this massacre is okay as there will only be one form active at a time!).
 
 for (var word of highlightedWords) {
     addListeners(word);
@@ -33,10 +36,17 @@ for (var word of highlightedWords) {
 
 function addListeners (word) {
     word.addEventListener("mouseenter", function(event) {
-        word.style.backgroundColor="rgba(169,225,196,0.8)";
+        if (formActive == true && highlightedKeyword != word) {
+            console.log("yolo");
+            okayToKillForm = true;
+            killForm(highlightedKeyword);
+        }
+        
+        highlightedKeyword = word;
+        word.style.backgroundColor="rgba(169,225,196,0.8)";        
+        
         if (!formActive) {
             var form = createForm(word);
-            console.log(word);
             
             form.addEventListener("mouseenter", function(event) {
                 okayToKillForm = false;
@@ -46,7 +56,7 @@ function addListeners (word) {
                 
                 setTimeout(function() {
                     killForm(word);
-                }, 250);
+                }, 200);
             });  
         }
         else {
@@ -57,35 +67,49 @@ function addListeners (word) {
         okayToKillForm = true;
         setTimeout(function() {
             killForm(word);
-        }, 250);       
+        }, 200);       
     });
 }
 
-//createForm() does what it says on the tin - it renders a form on the page using styles specified in the style.css file. The form_html array contains the inner html of the form, and is collapsed down using join('') to make it into a single string. This string is then added to a <div> element, which is given the class of 'jargon-bust-form'. We then insert the form before the first element in the body. This is okay as the form is absolutely positioned, and so shouldn't (in theory) damage the page layout.
+//createForm() does what it says on the tin - it renders a form on the page using styles specified in the style.css file. The form_html array contains the inner html of the form, and is collapsed down using join('') to make it into a single string. This string is then added to a <div> element, which is given the class of 'scout-js-form'. We then insert the form before the first element in the body. This is okay as the form is absolutely positioned, and so shouldn't (in theory) damage the page layout.
 
 function createForm (label) {
-    var star = chrome.extension.getURL("img/star2.png");
-    var browser = chrome.extension.getURL("img/www.png");
+    
+    var title = label.getAttribute('data-title');
+    var description = label.getAttribute('data-description');
+    var url = label.getAttribute('data-url');
     
     var form_html = [
-    '<h2>Wordpress</h2>',
-    '<p>Free and open-source web software that can be used to create  websites with dynamic functionality (e.g blogging, e-commerce). Wordpress is based on PHP and MySQL, and is the most popular blogging system in use on the Web.</p>',
-    '<a class="main-button" href="www.google.com">View on JargonBust.com</a>',
-    '<a class="icon" href="www.google.com">wordpress.org</a></div>'
+    '<h2>' + title + '</h2>',
+    '<p>' + description + '</p>',
+    '<a class="scout-js-main-button" href="#">Button to do something</a>',
+    '<a class="scout-js-meta" href="http://' + url + '" target="_blank">' + url + '</a></div>'
     ].join('');
     
     var form = document.createElement('div');
-    form.setAttribute('class', 'jargon-bust-form');
-    
-    var position = getCoords(label);
-    form.style.top=(position.top+28)+"px";
-    form.style.left=position.left+"px";
+    form.setAttribute('class', 'scout-js-form');
     
     form.innerHTML = form_html;    
     formActive = true;
-    okayToKillForm = true;
-    
+    okayToKillForm = true;    
+    form.style.visibility = "hidden";    
     document.body.insertBefore(form, document.body.firstChild);
+    
+    //In the lines directly above, we insert the pop-up form into the page as invisible. This is so that we can determine the height of the rendered pop-up. If the height of the pop-up is great than the distance between the keyword span and the bottom of the browser window, then we position the pop-up above the keyword (to avoid the pop-up running off the page). Otherwise, we position the pop-up below the keyword. We then make the pop-up visible.
+    
+    var position = getCoords(label);
+    if (form.offsetHeight >= window.innerHeight - label.offsetTop + label.offsetHeight + 10) {
+        //Put the pop-up above the keyword              
+        form.style.top=(position.top-form.offsetHeight - 10)+"px";
+        form.style.left=position.left+"px";
+    }
+    else {
+        //Put the pop-up below the keyword        
+        form.style.top=(position.top+28)+"px";
+        form.style.left=position.left+"px";
+    }
+    
+    form.style.visibility = "visible";
     return form;
 }
 
@@ -93,7 +117,7 @@ function createForm (label) {
 
 function killForm(word) {
     if (okayToKillForm) {                
-        var forms = document.body.getElementsByClassName("jargon-bust-form");
+        var forms = document.body.getElementsByClassName("scout-js-form");
         word.style.backgroundColor="rgba(169,225,196,0)";
         while(forms[0]) {       
             forms[0].parentNode.removeChild(forms[0]); 
@@ -102,7 +126,7 @@ function killForm(word) {
     }
 }
 
-// Function to get position of keyword labels on the page (thanks to Stack Overflow user 'basil' for this snippet - http://stackoverflow.com/questions/5598743/finding-elements-position-relative-to-the-document)
+// Function to get position of elements on the page (thanks to Stack Overflow user 'basil' for this snippet - http://stackoverflow.com/questions/5598743/finding-elements-position-relative-to-the-document)
 
 function getCoords(elem) { // crossbrowser version
     var box = elem.getBoundingClientRect();
